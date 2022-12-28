4c65af895afb720290527107106f2a5cf86a599a
your email address <your email address>
2022-12-28 16:45:21 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,70 +1,48 @@
-let todoArray = JSON.parse(localStorage.getItem("todoList"))
-  ? JSON.parse(localStorage.getItem("todoList"))
-  : [];
+let todoArray = JSON.parse(localStorage.getItem("todoList")) ? 
+  JSON.parse(localStorage.getItem("todoList")) : [];
 
 let input = document.querySelector(".input-item");
 let addButton = document.querySelector(".btn-add");
 let todoItem = document.querySelector(".content");
 
-// Button and function for adding items to the list 
+// Button to add items to the array 
 addButton.addEventListener("click", (e) => {
   e.preventDefault();
-  addTodo(input.value);
   duplicateItems(input.value);
-  // console.log(e.currentTarget);
-  // console.log([todoArray]);
   location.reload();
 });
-
-function addTodo(todoName) {
-  if (todoName !== "") {
-    let todoList = {
-      id: Date.now(),
-      name: todoName,
-      completed: false,
-    };
-    todoArray.push(todoList);
-    input.value = "";
-    localStorage.setItem("todoList", JSON.stringify(todoArray));
-  }
-}
 
 // Appending my items to the DOM
 function appendItems() {
   todoArray.forEach((item) => {
     let checked = item.completed ? "checked" : null;
+    let stylingClass = item.completed ? "line-through": null; 
 
-    todoItem.innerHTML += `
-            <div class="form-check items d-flex justify-content-between">
-                 <div data-key="${item.id}">
-                     <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" class="checkbox" ${checked}>
-                     <label class="form-check-label" for="flexCheckDefault" id="${item.id}">
-                     ${item.name}
-                     </label>
-                     </div>
-                     <button class="btn btn-success deleteBtn" id="${item.id}">X</button>
+    todoItem.innerHTML += 
+    `
+      <div class="form-check items d-flex justify-content-between">
+            <div data-key="${item.id}">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" class="checkbox" ${checked}>
+                <label class="form-check-label ${stylingClass}" for="flexCheckDefault" id="${item.id}">
+                ${item.name}
+                </label>
             </div>
-                     `;
-    let label = document.querySelectorAll("label");
-    label.forEach((labels) => {
-      if (item.completed == true) {
-        labels.classList.add("line-through");
-      }
-    });
+            <button class="btn btn-success deleteBtn" id="${item.id}">X</button>
+      </div>
+      `
   });
 }
 appendItems();
 
-// Delete Btn for the list of items 
+
+// Delete Button for the list of items and array
 let deleteBtn = document.querySelectorAll(".deleteBtn");
 deleteBtn.forEach((del, index) => {
   del.addEventListener("click", (e) => {
     e.preventDefault();
-    // console.log(e.currentTarget);
     todoArray.splice([index], 1);
     localStorage.setItem("todoList", JSON.stringify(todoArray));
     location.reload();
-    // console.log([todoArray]);
   });
 });
 
@@ -75,22 +53,9 @@ todoItem.addEventListener("click", (e) => {
 
   if (e.target.type === "checkbox") {
     toggle(e.target.parentElement.getAttribute("data-key"));
-    }       
-
+  }
   location.reload();
 });
-
-// let checkbox = document.querySelectorAll('.checkbox')
-// checkbox.forEach((checkboxes)=>{
-//     checkboxes.addEventListener('click', (e)=>{
-//         e.preventDefault();
-//         todoArray.forEach((id)=>{
-//             if (checkboxes.checked == true){
-//                 console.log('true');
-//             }
-//         })
-//     })
-// })
 
 // This function is to toggle between true and false for the completed property 
 function toggle(id) {
@@ -102,22 +67,24 @@ function toggle(id) {
   localStorage.setItem("todoList", JSON.stringify(todoArray));
 }
 
-// let label = document.querySelector('label');
-// // label.forEach((labels)=>{
+// Function for adding the items to the array as objects
+function addTodo(todoName) {
+  if (todoName !== "") {
+    let todoList = {
+      id: Date.now(),
+      name: todoName,
+      completed: false,
+    };
+    todoArray.push(todoList);
+    localStorage.setItem("todoList", JSON.stringify(todoArray));
+  }
+}
 
-//     label.addEventListener('click', (e)=>{
-//         e.preventDefault();
-//         console.log(e.currentTarget);
-//         label.classList.toggle("line-trough-styling");
-//     // })
-// })
-
-
-// The function is to prevent adding a duplicate item on the list (or at least it's suppose to)
+// The function is to prevent adding a duplicate item on the list.
 function duplicateItems(duplicate) {
-  todoArray.forEach((item) => {
-    if (item.name == duplicate) {
-      alert(`${item.name} is already on your list`);
+    if (todoArray.some(({name})=> name === duplicate)) {
+      alert(`${duplicate} is already on your list. Please enter a new item.`);
+    }else {
+      addTodo(input.value);
     }
-  });
 }
